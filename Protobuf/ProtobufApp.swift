3ab0ad102f541c81a8486abb5bd77c0cc29628a3
Jae-Seung Lee <jaeseung@gmail.com>
2022-09-12 17:07:41 -0400

    Initial Commit

---
+++ Protobuf/ProtobufApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtobufApp.swift
+//  Protobuf
+//
+//  Created by Jae Seung Lee on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ProtobufApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
