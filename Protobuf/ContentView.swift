d9078b0b2d1d8bc283509ea663a305a87187c110
Jae-Seung Lee <jaeseung@gmail.com>
2022-12-19 20:12:17 -0500

    Fix: Still TripUpdatesView popped up
    https://developer.apple.com/forums/thread/693137

--- Protobuf/ContentView.swift
+++ Protobuf/ContentView.swift
@@ -60,6 +60,7 @@ struct ContentView: View {
                         }
                     }
                 }
+                .navigationViewStyle(.stack)
             }
             
             Spacer()
