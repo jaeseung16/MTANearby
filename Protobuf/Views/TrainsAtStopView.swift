f3cb9d056f567e1866c28bb1a694aea982ec59d7
Jae-Seung Lee <jaeseung@gmail.com>
2023-01-09 17:07:04 -0500

    Refactor: Add RouteLabel

--- Protobuf/Views/TrainsAtStopView.swift
+++ Protobuf/Views/TrainsAtStopView.swift
@@ -45,7 +45,7 @@ struct TrainsAtStopView: View {
                                 EmptyView()
                             }
                         } label: {
-                            getLabel(for: train, trip: trip, arrivalTime: eventTime)
+                            label(for: train, trip: trip, arrivalTime: eventTime)
                         }
                     }
                 }
@@ -55,11 +55,11 @@ struct TrainsAtStopView: View {
         }
     }
     
-    private func getLabel(for train: MTATrain, trip: MTATrip, arrivalTime: Date) -> some View {
+    private func label(for train: MTATrain, trip: MTATrip, arrivalTime: Date) -> some View {
         HStack {
             Image(systemName: train.getDirection()?.systemName ?? "")
             
-            getRouteView(for: trip)
+            RouteLabel(trip: trip)
                 .frame(width: 30, height: 30)
             
             Spacer()
@@ -68,20 +68,9 @@ struct TrainsAtStopView: View {
                 Text(arrivalTime, style: .time)
                     .foregroundColor(.secondary)
             } else {
-                Text(getTimeInterval(arrivalTime))
+                Text(timeInterval(to: arrivalTime))
                     .foregroundColor(arrivalTime < Date() ? .secondary : .primary)
             }
-        }
-    }
-    
-    private func getRouteView(for trip: MTATrip) -> some View {
-        ZStack {
-            Circle()
-                .foregroundColor(getRouteColor(of: trip) ?? .clear)
-            
-            Text(trip.getRouteId()?.rawValue ?? "")
-                .font(.title2)
-                .foregroundColor(getRouteIdColor(of: trip))
         }
     }
     
@@ -89,45 +78,7 @@ struct TrainsAtStopView: View {
         return eventTime.timeIntervalSinceNow > viewModel.maxAgo && eventTime.timeIntervalSinceNow < viewModel.maxComing
     }
     
-    private func getRouteColor(of trip: MTATrip) -> Color? {
-        if let routeId = trip.getRouteId(), let mtaRoute = ViewModel.mtaRoutes.first(where: { $0.id == routeId }) {
-            return Color(uiColor: hexStringToUIColor(mtaRoute.color))
-        }
-        return nil
-    }
-    
-    private func getRouteIdColor(of trip: MTATrip) -> Color {
-        switch trip.getRouteId() {
-        case .n, .q, .r, .w:
-            return .black
-        default:
-            return .white
-        }
-    }
-    
-    private func hexStringToUIColor(_ hex: String) -> UIColor {
-        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-
-        if (cString.hasPrefix("#")) {
-            cString.remove(at: cString.startIndex)
-        }
-
-        if ((cString.count) != 6) {
-            return UIColor.gray
-        }
-
-        var rgbValue:UInt64 = 0
-        Scanner(string: cString).scanHexInt64(&rgbValue)
-
-        return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
-    }
-    
-    private func getTimeInterval(_ arrivalTime: Date) -> String {
+    private func timeInterval(to arrivalTime: Date) -> String {
         return RelativeDateTimeFormatter().localizedString(for: arrivalTime, relativeTo: Date())
     }
 }
